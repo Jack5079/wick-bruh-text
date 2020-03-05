bbfa2379b0322d3f823b19511554384ba7088bb8
Jack <29169102+Jack5079@users.noreply.github.com>
2020-03-05 14:37:09 +0000

    Also replace sounds with bruh

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ async function addProject (proj) {
   if (
     proj.name.endsWith('.wick')
   ) {
-    const zip = new JSZip()
-    const folder = await zip.loadAsync(await proj.arrayBuffer())
+    const sound = await (await fetch('./sound.mp3')).blob()
+    const folder = await new JSZip().loadAsync(await proj.arrayBuffer())
     const json = JSON.parse(await folder.files['project.json'].async('string'))
 
     Object.keys(json.objects).filter(id => {
@@ -13,7 +13,10 @@ async function addProject (proj) {
     }).forEach(id => {
       json.objects[id].json[1].content = 'bruh'
     })
-    const blob = await zip.file('project.json', JSON.stringify(json)).generateAsync({ type: 'blob' })
+    Object.keys(folder.files).filter(e=>e.startsWith('assets/') && e.endsWith('.mp3')).forEach(id=>{
+        folder.file(id, sound)
+    })
+    const blob = await folder.file('project.json', JSON.stringify(json)).generateAsync({ type: 'blob' })
 
     const link = document.createElement('a')
     link.href = URL.createObjectURL(blob)
