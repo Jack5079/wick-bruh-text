56d3772fac0214acec9cb34d994e94a07084451d
Jack <29169102+Jack5079@users.noreply.github.com>
2020-03-05 13:39:23 -0500

    better sound

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-/* global JSZip */
+/* global JSZip, fetch */
+
 async function addProject (proj) {
   if (
     proj.name.endsWith('.wick')
   ) {
-    const sound = await (await fetch('./sound.mp3')).blob()
     const folder = await new JSZip().loadAsync(await proj.arrayBuffer())
-    const json = JSON.parse(await folder.files['project.json'].async('string'))
+    const json = JSON.parse(await folder.files['project.json'].async('string')) // .wick files are just .zip files. most of the data is in project.json
 
-    Object.keys(json.objects).filter(id => {
-      if (!json.objects[id].json) return false
-      return json.objects[id].json[0] === 'PointText'
-    }).forEach(id => {
+    // this replaces every text with "bruh"
+    Object.keys(json.objects).filter(id => { // Clips, text, paths, etc. are stored in the project.json file. We will get the IDs of every single object, and filter it.
+      if (!json.objects[id].json) return false // If it doesn't have the data we need, ignore it.
+      return json.objects[id].json[0] === 'PointText' // If does have the data but it isn't text, ignore it.
+    }).forEach(id => { // Now for the actual text replacement
       json.objects[id].json[1].content = 'bruh'
     })
-    Object.keys(folder.files).filter(e=>e.startsWith('assets/') && e.endsWith('.mp3')).forEach(id=>{
-        folder.file(id, sound)
-    })
+
+    // this replaces all .mp3 assets with the bruh sound
+    const mp3files = Object.keys(folder.files)
+      .filter(e => e.startsWith('assets/') && e.endsWith('.mp3')) // .mp3 files in the assets folder
+    if (mp3files.length) {
+      const sound = await (await fetch('./sound.mp3')).blob()
+      mp3files.forEach(id => {
+        folder.file(id, sound) // Overwrite the files with the bruh sound blob.
+      })
+    }
     const blob = await folder.file('project.json', JSON.stringify(json)).generateAsync({ type: 'blob' })
 
     const link = document.createElement('a')
